89197a71462107c2d5f31e144ac356a6c1d563f5
absol <absolutegoddus@protonmail.com>
2024-06-23 21:53:11 +0200

    Fixed Scene.hpp

--- engine/lib/Scene.hpp
+++ engine/lib/Scene.hpp
@@ -14,35 +14,35 @@ namespace engine {
         ObjectList<GameObject> Objects;
 
         void init() {
-            for (auto o : Objects) {
+            for (auto &o : Objects) {
                 o.init();
             }
         }
 
         void update() {
-            for (auto o : Objects) {
+            for (auto &o  : Objects) {
                 o.preUpdate();
             }
 
-            for (auto o : Objects) {
+            for (auto &o : Objects) {
                 o.update();
             }
 
-            for (auto o : Objects) {
+            for (auto &o : Objects) {
                 o.postUpdate();
             }
         }
 
         void cleanup() {
-            for (auto o : Objects) {
-                engine::Event e("Cleanup", "Cleanup due to call of 'cleanup()' in Scene.hpp:35");
+            for (auto &o : Objects) {
+                engine::Event e("SCENE_Cleanup", "Cleanup due to call of 'cleanup()' in Scene.hpp:36");
                 o.onEvent(e);
             }
         }
 
         ~Scene() {
             for (auto o : Objects) {
-                engine::Event e("Destroy", "Destroy due to deletion of Scene in Scene.hpp:41");
+                engine::Event e("SCENE_Destroy", "Destroy due to deletion of Scene in Scene.hpp:43");
                 o.onEvent(e);
             }
         }
